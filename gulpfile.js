c6abff1bb1983c8db6eae31195ca06da5ba067d3
Markuz GJ <markuz.br@gmail.com>
2014-12-19 18:06:00 -0200

    turn seed project into package

--- gulpfile.js
+++ gulpfile.js
@@ -6,75 +6,17 @@ var del = require('del');
 var runSequence = require('run-sequence');
 var browserSync = require('browser-sync');
 var reload = browserSync.reload;
+var assign = require('object-assign')
 
-var watch = require('./etc/watchers');
-var CFG = require('./etc/config');
-
-var log = $.util.log
-var red = $.util.colors.red
-var cyan = $.util.colors.cyan
-var mag = $.util.colors.magenta
+var CFG = require('./tasks/config');
 
 var BS
 var SERVE = false
 
+
+module.exports = function gulpfile (userGulp) {
+  assign(userGulp, gulp)
+}
+
 // Load custom tasks from the `tasks` directory
-try { require('require-dir')('etc'); } catch (err) { console.error(err) }
-
-// Clean Output Directory
-gulp.task('clean', del.bind(null, [CFG.tmp, CFG.build], {force: true}));
-
-// TODO: add comments
-gulp.task('default', ['build'])
-
-// TODO: add comments
-gulp.task('dev', ['clean'], function(next){
-  runSequence('assets', function(){
-    if (browserSync.active) { gulp.start('reload') }
-    next()
-  })
-})
-
-// Lint JavaScript
-gulp.task('jshint', function () {
-  return gulp.src(CFG.js.src)
-    .pipe(reload({stream: true, once: true}))
-    .pipe($.jshint())
-    .pipe($.jshint.reporter('jshint-stylish'))
-    .pipe($.if(!browserSync.active, $.jshint.reporter('fail')));
-});
-
-// TODO: add comments
-gulp.task('serve', function (next) {
-  SERVE = true
-  CFG.browserSync.browser = CFG.browserSync.browser || 'skip'
-  browserSync(CFG.browserSync, function(err, bs){
-    if (err) {throw err}
-    BS = bs
-    log("Loaded '"+ cyan('browserSync') +"'...")
-    next()
-  });
-});
-
-// TODO: add comments
-gulp.task('reload', function(next){
-  if (!SERVE) return next()
-
-  browserSync.reload()
-  BS.logger.info('Local URL: '+ mag(BS.options.urls.local))
-  BS.logger.info('External URL: '+ mag(BS.options.urls.external))
-  next()
-});
-
-// TODO: add comments
-gulp.task('restart', function(){
-  log(red(':: restarting ::'))
-  process.exit(0)
-})
-
-// // TODO: add comments
-gulp.task('watch', function(next){
-  watch.gulpfile()
-  watch.assets()
-  next()
-})
+try { require('require-dir')('tasks'); } catch (err) { console.error(err) }
