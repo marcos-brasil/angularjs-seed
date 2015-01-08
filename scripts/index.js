1d006584a544acea64193265fc9311eccbd3a37e
Markuz GJ <markuz.br@gmail.com>
2015-01-08 12:54:09 -0800

    fix

--- scripts/index.js
+++ scripts/index.js
@@ -400,7 +400,7 @@ function rootAppCtrl($scope, $q, $sce, $state) {
             context$2$0.next = 12;
             break;
           }
-          return context$2$0.delegateYield(_waitNextFrame(FPS), "t33", 6);
+          return context$2$0.delegateYield(_waitNextFrame(FPS), "t35", 6);
         case 6:
           context$2$0.next = 8;
           return new Promise(function (res) {
@@ -504,8 +504,6 @@ function indexCtrl($scope, $root, $q, $sce, $state) {
           context$2$0.next = 2;
           return new Promise(setImmediate);
         case 2:
-          console.log("indexStateCtrl");
-        case 3:
         case "end":
           return context$2$0.stop();
       }
