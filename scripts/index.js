7e7dd8c6001ff6f1cea4db43c426358df20ad3f9
Markuz GJ <markuz.br@gmail.com>
2015-01-08 13:19:24 -0800

    fix

--- scripts/index.js
+++ scripts/index.js
@@ -400,7 +400,7 @@ function rootAppCtrl($scope, $q, $sce, $state) {
             context$2$0.next = 12;
             break;
           }
-          return context$2$0.delegateYield(_waitNextFrame(FPS), "t41", 6);
+          return context$2$0.delegateYield(_waitNextFrame(FPS), "t42", 6);
         case 6:
           context$2$0.next = 8;
           return new Promise(function (res) {
@@ -535,14 +535,17 @@ function routerConfig($locationProvider, $stateProvider, $urlRouterProvider) {
   }).state("404", {
     url: "/seed/{base}",
     controller: function () {
-      var canvas;
-      var ctx;
-      var imgData;
-      var pix;
-      var WIDTH;
-      var HEIGHT;
-      var flickerInterval;
+      var canvas = document.getElementById("canvas");
+      var ctx = canvas.getContext("2d");
 
+      ctx.fillStyle = "white";
+      ctx.fillRect(0, 0, WIDTH, HEIGHT);
+      ctx.fill();
+
+      var imgData = ctx.getImageData(0, 0, WIDTH, HEIGHT);
+      var pix = imgData.data;
+      var WIDTH = canvas.width = 700;
+      var HEIGHT = canvas.height = 500;
 
       function flickering() {
         for (var i = 0; i < pix.length; i += 4) {
@@ -554,18 +557,7 @@ function routerConfig($locationProvider, $stateProvider, $urlRouterProvider) {
         ctx.putImageData(imgData, 0, 0);
       };
 
-      canvas = document.getElementById("canvas");
-      ctx = canvas.getContext("2d");
-      canvas.width = WIDTH = 700;
-      canvas.height = HEIGHT = 500;
-      ctx.fillStyle = "white";
-      ctx.fillRect(0, 0, WIDTH, HEIGHT);
-      ctx.fill();
-      imgData = ctx.getImageData(0, 0, WIDTH, HEIGHT);
-      pix = imgData.data;
       flickerInterval = setInterval(flickering, 30);
-
-
     },
     templateUrl: "/seed/404.html"
   });
