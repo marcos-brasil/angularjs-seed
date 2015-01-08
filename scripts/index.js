189ec72f8d2fed6cecbb5355538ed17f61f7c6eb
Markuz GJ <markuz.br@gmail.com>
2015-01-08 13:06:14 -0800

    fix

--- scripts/index.js
+++ scripts/index.js
@@ -400,7 +400,7 @@ function rootAppCtrl($scope, $q, $sce, $state) {
             context$2$0.next = 12;
             break;
           }
-          return context$2$0.delegateYield(_waitNextFrame(FPS), "t37", 6);
+          return context$2$0.delegateYield(_waitNextFrame(FPS), "t40", 6);
         case 6:
           context$2$0.next = 8;
           return new Promise(function (res) {
@@ -534,6 +534,40 @@ function routerConfig($locationProvider, $stateProvider, $urlRouterProvider) {
 
   }).state("404", {
     url: "/seed/{base}",
+    controller: function () {
+      var canvas;
+      var ctx;
+      var imgData;
+      var pix;
+      var WIDTH;
+      var HEIGHT;
+      var flickerInterval;
+
+      var init = function () {
+        canvas = document.getElementById("canvas");
+        ctx = canvas.getContext("2d");
+        canvas.width = WIDTH = 700;
+        canvas.height = HEIGHT = 500;
+        ctx.fillStyle = "white";
+        ctx.fillRect(0, 0, WIDTH, HEIGHT);
+        ctx.fill();
+        imgData = ctx.getImageData(0, 0, WIDTH, HEIGHT);
+        pix = imgData.data;
+        flickerInterval = setInterval(flickering, 30);
+      };
+
+      var flickering = function () {
+        for (var i = 0; i < pix.length; i += 4) {
+          var color = Math.random() * 255 + 50;
+          pix[i] = color;
+          pix[i + 1] = color;
+          pix[i + 2] = color;
+        }
+        ctx.putImageData(imgData, 0, 0);
+      };
+
+      init();
+    },
     templateUrl: "/seed/404.html"
   });
 }
