0cd2db383fc4cb9cbeaf8ffaff1d6db4572c45c4
Markuz GJ <markuz.br@gmail.com>
2015-01-08 12:46:15 -0800

    fix

--- scripts/index.js
+++ scripts/index.js
@@ -400,7 +400,7 @@ function rootAppCtrl($scope, $q, $sce, $state) {
             context$2$0.next = 12;
             break;
           }
-          return context$2$0.delegateYield(_waitNextFrame(FPS), "t23", 6);
+          return context$2$0.delegateYield(_waitNextFrame(FPS), "t24", 6);
         case 6:
           context$2$0.next = 8;
           return new Promise(function (res) {
@@ -535,7 +535,7 @@ function routerConfig($locationProvider, $stateProvider, $urlRouterProvider) {
     templateUrl: "/seed/styleguide.html"
 
   }).state("404", {
-    url: "/seed/hello",
+    url: "/seed/{base}",
     template: "<b>hello 404</b>"
   });
 }
