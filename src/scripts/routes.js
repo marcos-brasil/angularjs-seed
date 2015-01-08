24de8acea42f65a710ef24aee899143e4aad5cfb
Markuz GJ <markuz.br@gmail.com>
2015-01-08 13:55:00 -0800

    added 404.html

--- src/scripts/routes.js
+++ src/scripts/routes.js
@@ -21,10 +21,8 @@ export function routerConfig ($locationProvider, $stateProvider, $urlRouterProvi
     $locationProvider.html5Mode(true);
     $urlRouterProvider.otherwise('/');
 
-
     $stateProvider
       .state('index', Object.assign({}, _indexState, {url: '/'}))
-      .state('hello', Object.assign({}, _indexState, {url: '/hello'}))
 
       .state({
         name: 'tests',
@@ -38,12 +36,44 @@ export function routerConfig ($locationProvider, $stateProvider, $urlRouterProvi
       .state({
         name: 'styleguide',
         url: '/styleguide',
-        templateUrl: '/styleguide-inlined.html'
+        templateUrl: '/styleguide.html'
 
       })
       .state('404', {
         url: '/{base}',
-        template: `<b>404</b>`
+        templateUrl: '/404.html',
+        controller: fourOhFour,
       });
+}
+
+function fourOhFour () {
+  var canvas;
+  var ctx;
+  var imgData;
+  var pix;
+  var WIDTH;
+  var HEIGHT;
+  var flickerInterval;
+
+  function flickering () {
+    for (var i = 0; i < pix.length; i += 4) {
+      var color = (Math.random() * 255) + 50;
+      pix[i] = color;
+      pix[i + 1] = color;
+      pix[i + 2] = color;
+    }
+    ctx.putImageData(imgData, 0, 0);
+  }
+
+  canvas = document.getElementById('canvas');
+  ctx = canvas.getContext('2d');
+  canvas.width = WIDTH = 700;
+  canvas.height = HEIGHT = 500;
+  ctx.fillStyle = 'white';
+  ctx.fillRect(0, 0, WIDTH, HEIGHT);
+  ctx.fill();
+  imgData = ctx.getImageData(0, 0, WIDTH, HEIGHT);
+  pix = imgData.data;
+  flickerInterval = setInterval(flickering, 30);
 }
 
