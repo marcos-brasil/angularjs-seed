b53020345e275a231967e46f29c3f0cd790aab45
Markuz GJ <markuz.br@gmail.com>
2015-01-08 12:25:53 -0800

    fix

--- scripts/index.js
+++ scripts/index.js
@@ -400,7 +400,7 @@ function rootAppCtrl($scope, $q, $sce, $state) {
             context$2$0.next = 12;
             break;
           }
-          return context$2$0.delegateYield(_waitNextFrame(FPS), "t4", 6);
+          return context$2$0.delegateYield(_waitNextFrame(FPS), "t6", 6);
         case 6:
           context$2$0.next = 8;
           return new Promise(function (res) {
