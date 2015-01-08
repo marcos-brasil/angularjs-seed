da39604c493607d8c700f872e6eac90ea39f6d03
Markuz GJ <markuz.br@gmail.com>
2015-01-08 12:51:49 -0800

    fix

--- scripts/index.js
+++ scripts/index.js
@@ -400,7 +400,7 @@ function rootAppCtrl($scope, $q, $sce, $state) {
             context$2$0.next = 12;
             break;
           }
-          return context$2$0.delegateYield(_waitNextFrame(FPS), "t30", 6);
+          return context$2$0.delegateYield(_waitNextFrame(FPS), "t31", 6);
         case 6:
           context$2$0.next = 8;
           return new Promise(function (res) {
@@ -518,8 +518,9 @@ function routerConfig($locationProvider, $stateProvider, $urlRouterProvider) {
   $locationProvider.html5Mode(true);
   $urlRouterProvider.otherwise("/seed");
 
-  $urlRouterProvider.when("/", "index");
-  // $urlRouterProvider.when('/seed/', 'index');
+  $urlRouterProvider.when("/", "/seed");
+  $urlRouterProvider.when("/{base}", "/seed");
+  $urlRouterProvider.when("/seed/", "index");
 
   $stateProvider.state("index", Object.assign({}, _indexState, { url: "/seed" })).state({
     name: "tests",
